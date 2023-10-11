6c8c15c832903ed49b4e31ff12c9402abd701082
humaira <humaira.tabassumkhan@gmail.com>
2023-10-11 11:47:16 +0600

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Dr. Nice' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+ 
+];
